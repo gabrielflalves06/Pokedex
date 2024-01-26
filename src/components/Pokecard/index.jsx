4ebbaf2516ed7ebd36803f282981aa665a8e0708
gabrielflalves06 <gabrielferreiraalves41@gmail.com>
2024-01-25 21:40:54 -0300

    update

--- src/components/Pokecard/index.jsx
+++ src/components/Pokecard/index.jsx
@@ -2,17 +2,22 @@ import './index.css'
 
 const PokeCard = (props) => {
     const { id, nome, imagem, tipos } = props;
+
+    const primeiroTipo = tipos.length > 0 ? tipos[0].type.name : '';
   
+    // Mapeia os tipos para exibir na tela
+    const tiposExibidos = tipos.map((type) => type.type.name.toUpperCase()).join(' / ');
+
+    const cardClasses = `card_pokemon ${primeiroTipo}`;
+
     return (
-      <div className="card_pokemon">
+      <div className={cardClasses}>
         <h4>#{id}</h4>
         <h3>{nome}</h3>
         <img src={imagem} alt="" />
-        {tipos.map((type) => (
-          <p className="type">
-            {type.type.name}
-          </p>
-        ))}
+          <h4 className="type">
+            {tiposExibidos}
+          </h4>
       </div>
     );
   };
